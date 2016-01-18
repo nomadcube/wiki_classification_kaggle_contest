53b51b2f23ce844feb14f93421c3179a4299a75a
西伯利亚游牧人 <nomadcube@gmail.com>
2016-01-18 17:25:52 +0800

    优化数据读入代码

--- data_processing/transformation.py
+++ data_processing/transformation.py
@@ -1,138 +1,42 @@
-import linecache
+import itertools
+
 from collections import namedtuple
-from random import random, randrange
 
 from scipy.sparse import csr_matrix
 
-from data_processing.tf_idf import x_with_tf_idf
-
-from memory_profiler import profile
+from pympler.asizeof import asizeof
 
 
-def sample_line_num(max_line_num, sample_size):
-    res = set()
-    while len(res) < sample_size:
-        new_line_num = randrange(1, max_line_num)
-        if new_line_num not in res:
-            res.add(new_line_num)
-    return res
-
-
-def sample_reader(data_file_path, max_line_num, sample_size):
+def sample_reader(data_file_path, sample_size):
     """
     Read data_processing from data_file_path and convert it to a Sample object.
 
     :param data_file_path: str
-    :param max_line_num: int
     :param sample_size: int
     :return: sample
     """
     sample = namedtuple('sample', 'y x')
     y = list()
-    x = list()
-    selected_line_no = sample_line_num(max_line_num, sample_size)
-    for line_no in selected_line_no:
-        line = linecache.getline(data_file_path, line_no)
-        tmp_y, tmp_x = line.strip().split(' ', 1)
-        y.append([int(tmp_label) for tmp_label in tmp_y.split(',')])
-        instance = dict()
-        for column in tmp_x.split(' '):
-            k, v = column.split(':')
-            instance[int(k)] = int(v)
-        x.append(instance)
+    element_x = list()
+    row_index_x = list()
+    col_index_x = list()
+    with open(data_file_path) as f:
+        for line_no, line in enumerate(itertools.islice(f.__iter__(), sample_size)):
+            multi_label, instance = line.strip().split(' ', 1)
+            y.append([int(label) for label in multi_label.split(',')])
+            for feature in instance.split(' '):
+                column, element = feature.split(':')
+                element_x.append(int(element))
+                col_index_x.append(int(column))
+                row_index_x.append(int(line_no))
+    x = csr_matrix((element_x, (row_index_x, col_index_x)), shape=(max(row_index_x) + 1, max(col_index_x) + 1),
+                   dtype='int32')
     return sample(y, x)
 
 
-def dimension_reduction(x, threshold):
-    """
-    Use tf-idf to perform dimension reduction, update x and y.
-
-    :param x: namedtuple
-    :param threshold: float
-    :return: Sample
-    """
-    return x_with_tf_idf(x, threshold)
-
-
-def construct_csr_sample(base_x, feature_mapping_relation=None):
-    """
-    Convert to csr format with feature remapped to col index.
-
-    :param base_x: list
-    :param feature_mapping_relation: {None, dict}
-    :return csr_matrix
-    """
-    if not feature_mapping_relation:
-        data = list()
-        row_ind = list()
-        col_ind = list()
-        feature_mapping = dict()
-        for instance_id, instance in enumerate(base_x):
-            if len(instance) == 0:
-                continue
-            for feature, feature_val in instance.items():
-                row_ind.append(instance_id)
-                feature_num = len(feature_mapping)
-                feature_mapping.setdefault(feature, feature_num)
-                col_ind.append(feature_mapping[feature])
-                data.append(feature_val)
-        return csr_matrix((data, (row_ind, col_ind)), shape=(len(base_x), len(feature_mapping))), feature_mapping
-    else:
-        data = list()
-        row_ind = list()
-        col_ind = list()
-        for instance_id, instance in enumerate(base_x):
-            if len(instance) == 0:
-                continue
-            for feature in feature_mapping_relation.keys():
-                row_ind.append(instance_id)
-                col_ind.append(feature_mapping_relation[feature])
-                val = instance[feature] if instance.get(feature) is not None else 0.0
-                data.append(val)
-        return csr_matrix((data, (row_ind, col_ind)), shape=(len(base_x), max(col_ind) + 1))
-
-
-def flatting_multi_label(original_y, original_x):
-    if len(original_x) != len(original_y):
-        raise ValueError('Length of original_y and original_y must be the same.')
-    new_y = list()
-    new_x = list()
-    flatting_rel = dict()
-    new_instance_id = 0
-    for instance_id in range(len(original_y)):
-        flatting_rel.setdefault(instance_id, set())
-        each_original_y = original_y[instance_id]
-        each_original_x = original_x[instance_id]
-        labels = original_y[instance_id].split(',')
-        if len(labels) <= 1:
-            new_y.append(each_original_y)
-            new_x.append(each_original_x)
-            flatting_rel[instance_id].add(new_instance_id)
-            new_instance_id += 1
-        else:
-            for each_label in labels:
-                new_y.append(each_label)
-                new_x.append(each_original_x)
-                flatting_rel[instance_id].add(new_instance_id)
-                new_instance_id += 1
-    return new_y, new_x, flatting_rel
-
-
-def assemble_y(flat_y, flat_relation):
-    new_y = list()
-    for y_collection_index in flat_relation.values():
-        tmp_y = set()
-        for i in y_collection_index:
-            tmp_y.add(flat_y[i])
-        new_y.append(','.join(tmp_y))
-    return new_y
-
-
 if __name__ == '__main__':
-    print(construct_csr_sample([{'1250536': 1},
-                                {},
-                                {'1250536': 1, '805104': 1}]))
-    import numpy as np
-
-    X = np.random.random_sample([2, 10])
-    np.dot(X, X.T)
+    smp = sample_reader('/Users/wumengling/PycharmProjects/kaggle/unit_test_data/sample.txt', 2)
+    print(smp.y)
+    print(smp.x)
+    print(smp.x.dtype)
+    print(asizeof(smp.x))
--- learning/multinomial_naive_bayes.py
+++ learning/multinomial_naive_bayes.py
@@ -7,33 +7,10 @@ path_train_sample = sys.argv[1] if len(
         sys.argv) >= 2 else '/Users/wumengling/PycharmProjects/kaggle/input_data/train.csv'
 
 max_line = 2365436
-sample_size = 1000000
+sample_size = 100000
 
 start_time = time()
-samples = sample_reader(path_train_sample, max_line, sample_size)
+samples = sample_reader(path_train_sample, sample_size)
 print(time() - start_time)
 print(type(samples.y))
 print(len(samples.y))
-
-
-# dimension_reduced_x = x_with_tf_idf(sample.x, learning_config['tf_idf_threshold']) if \
-#     learning_config['tf_idf_threshold'] > 0 else sample.x
-
-# train_y, train_x, test_y, test_x = split_train_test(dimension_reduced_x, sample.y, learning_config['split_proportion'])
-# train_desc = describe_x_y(train_x, train_y)
-# learning_config['train_sample_size'] = train_desc.sample_size
-# learning_config['train_feature_dimension'] = train_desc.feature_dimension
-
-# csr_train_x, csr_feature_mapping_rel = construct_csr_sample(train_x)
-#
-# all_classes = train_desc.class_distribution.keys()
-# nb_learner = MultiOutputMultinomialNB()
-# for chuck_train_y, chuck_train_x in chunking.iter_chuck(train_y.__iter__(), train_x.__iter__(), 200000):
-#     flat_csr_train_x = construct_csr_sample(chuck_train_x, csr_feature_mapping_rel)
-#     nb_learner.partial_fit(flat_csr_train_x, chuck_train_y, classes=all_classes)
-#
-# predicted_y_train = nb_learner.multi_predict(csr_train_x, 3)
-# print(macro_precision_and_recall([','.join(each_y) for each_y in train_y], predicted_y_train))
-#
-# predicted_y_test = nb_learner.multi_predict(construct_csr_sample(test_x, csr_feature_mapping_rel), 3)
-# print(macro_precision_and_recall([','.join(each_y) for each_y in test_y], predicted_y_test))
