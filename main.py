f6c2da52489f76512702bbe565824ea959b8647c
西伯利亚游牧人 <nomadcube@gmail.com>
2016-01-19 15:35:52 +0800

    delete some useless files

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from cs_tf_idf import tf_idf
 
 train_sample_path = sys.argv[1] if len(
         sys.argv) >= 2 else '/Users/wumengling/PycharmProjects/kaggle/input_data/train.csv'
-sample_size = sys.argv[2] if len(sys.argv) >= 3 else 1000
+sample_size = sys.argv[2] if len(sys.argv) >= 3 else 10
 
 sample = sample_reader(train_sample_path, sample_size)
 x_tf_idf = tf_idf(sample.x)
