e7524f66d4576b0060eeca4e399626c93927a9d7
西伯利亚游牧人 <nomadcube@gmail.com>
2016-01-20 16:52:14 +0800

    由于内存不足，改成增量算法

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 import sys
 from time import time
 
@@ -5,20 +6,10 @@ from reader import sample_reader
 
 train_sample_path = sys.argv[1] if len(
         sys.argv) >= 2 else '/Users/wumengling/PycharmProjects/kaggle/input_data/train.csv'
-sample_size = int(sys.argv[2]) if len(sys.argv) >= 3 else 10
+sample_size = int(sys.argv[2]) if len(sys.argv) >= 3 else 5
 
 read_start_time = time()
-sample = sample_reader(train_sample_path, sample_size)
+sample_1, sample_2 = sample_reader(train_sample_path, 0, math.floor(sample_size / 2.0), sample_size)
 print("time used for sample_reader is {0}".format(time() - read_start_time))
-print(len(sample[0]))
-print(len(sample[1]))
-print(len(sample[2]))
-print(len(sample[3]))
-
-# tf_idf_start_time = time()
-# x_tf_idf = tf_idf(sample.x)
-# print("time used for tf_idf is {0}".format(time() - tf_idf_start_time))
-#
-# print(sample.x)
-# print(x_tf_idf.shape)
-# print(x_tf_idf.nnz)
+print(len(sample_1.y))
+print(len(sample_2.y))
--- reader.py
+++ reader.py
@@ -1,47 +1,40 @@
 import itertools
-from collections import namedtuple
 
 
-def describe(data_file_path):
-    dimension = 0
+class Sample:
+    def __init__(self):
+        self.y = list()
+        self.element_x = list()
+        self.row_index_x = list()
+        self.col_index_x = list()
+
+    def increment_by_one_line(self, line_no, line):
+        multi_label, instance = line.strip().split(' ', 1)
+        self.y.append([int(label) for label in multi_label.split(',')])
+        for feature in instance.split(' '):
+            column, element = feature.split(':')
+            self.element_x.append(int(element))
+            self.col_index_x.append(int(column))
+            self.row_index_x.append(int(line_no))
+        return self
+
+
+def sample_reader(data_file_path, begin_line_no, split_line_no, end_line_no):
+    part_one_sample = Sample()
+    part_two_sample = Sample()
     with open(data_file_path) as f:
-        for line_no, line in enumerate(f):
-            y, x = line.split(' ', 1)
-            for feature in x.split(' '):
-                col = int(feature.split(':')[0])
-                if col > dimension:
-                    dimension = col
-    return dimension
-
-
-def sample_reader(data_file_path, sample_size):
-    """
-    Read data_processing from data_file_path and convert it to a Sample object.
-
-    :param data_file_path: str
-    :param sample_size: int
-    :return: sample
-    """
-    sample = namedtuple('sample', 'y x')
-    y = list()
-    element_x = list()
-    row_index_x = list()
-    col_index_x = list()
+        for line_no, line in enumerate(itertools.islice(f.__iter__(), begin_line_no, split_line_no)):
+            part_one_sample.increment_by_one_line(line_no, line)
     with open(data_file_path) as f:
-        for line_no, line in enumerate(itertools.islice(f.__iter__(), sample_size)):
-            multi_label, instance = line.strip().split(' ', 1)
-            y.append([int(label) for label in multi_label.split(',')])
-            for feature in instance.split(' '):
-                column, element = feature.split(':')
-                element_x.append(int(element))
-                col_index_x.append(int(column))
-                row_index_x.append(int(line_no))
-    return y, element_x, col_index_x, row_index_x
+        for line_no, line in enumerate(itertools.islice(f.__iter__(), split_line_no, end_line_no)):
+            part_two_sample.increment_by_one_line(line_no, line)
+    return part_one_sample, part_two_sample
 
 
 if __name__ == '__main__':
-    smp = sample_reader('/Users/wumengling/PycharmProjects/kaggle/unit_test_data/sample.txt', 2)
-    # print(smp.y)
-    # print(smp.x)
-    # print(smp.x.dtype)
-    # print(asizeof(smp.x))
+    smp_1, smp_2 = sample_reader('/Users/wumengling/PycharmProjects/kaggle/unit_test_data/sample.txt', 0, 2, 3)
+    print(smp_1.y)
+    print(smp_2.y)
+
+    print(smp_1.element_x)
+    print(smp_2.element_x)
