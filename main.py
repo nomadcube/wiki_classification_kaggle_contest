9dac701c36a034dafec0bb15cf2fbaf79ed37dff
西伯利亚游牧人 <nomadcube@gmail.com>
2016-01-20 13:26:12 +0800

    优化数据读入代码

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from reader import sample_reader
 
 train_sample_path = sys.argv[1] if len(
         sys.argv) >= 2 else '/Users/wumengling/PycharmProjects/kaggle/input_data/train.csv'
-sample_size = sys.argv[2] if len(sys.argv) >= 3 else 10
+sample_size = int(sys.argv[2]) if len(sys.argv) >= 3 else 10
 
 sample = sample_reader(train_sample_path, sample_size)
 x_tf_idf = tf_idf(sample.x)
