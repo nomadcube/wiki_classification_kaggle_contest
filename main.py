8166f4b4492df0e13e45f16db8f844a1c8f0cfb8
西伯利亚游牧人 <nomadcube@gmail.com>
2016-01-21 15:35:22 +0800

    fit in mnb

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 from time import time
 
 from scipy.sparse import csr_matrix
+from sklearn.naive_bayes import MultinomialNB
 
 import reader
 import sparse_tf_idf
@@ -10,10 +11,19 @@ sample_path = sys.argv[1] if len(sys.argv) > 1 else '/Users/wumengling/PycharmPr
 size_of_sample = int(sys.argv[2]) if len(sys.argv) > 2 else 3
 
 start_time = time()
+
+# read data from file
 smp = reader.read_sample(sample_path, size_of_sample)
 smp_x = csr_matrix((smp.element_x, (smp.row_index_x, smp.col_index_x)),
                    shape=(max(smp.row_index_x) + 1, max(smp.col_index_x) + 1))
+
+# perform tf-idf
 tf_idf_smp_x = sparse_tf_idf.tf_idf(smp_x)
-print(tf_idf_smp_x.shape)
-print(len(smp.y))
+
+# fit mnb model
+mnb = MultinomialNB()
+mnb.fit(tf_idf_smp_x, smp.y)
+predicted_y = mnb.predict(tf_idf_smp_x)
+print(predicted_y[:3])
+print(smp.y[:3])
 print(time() - start_time)
