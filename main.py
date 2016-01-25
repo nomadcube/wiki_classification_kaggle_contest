ac042368a1722cb9146d158b69fb1b3f02377113
西伯利亚游牧人 <nomadcube@gmail.com>
2016-01-25 12:38:54 +0800

    修复训练数据和测试数据维度不一致的问题

--- main.py
+++ main.py
@@ -14,22 +14,30 @@ size_of_train_sample = int(sys.argv[3]) if len(sys.argv) > 3 else 5
 start_time = time()
 
 # read data from file
+print(repr(time()) + ': read data starting')
 train_smp, test_smp = reader.read_sample(sample_path, size_of_sample, size_of_train_sample)
 n_dim = max(max(train_smp.col_index_x), max(test_smp.col_index_x)) + 1
 train_x = csr_matrix((train_smp.element_x, (train_smp.row_index_x, train_smp.col_index_x)),
                      shape=(max(train_smp.row_index_x) + 1, n_dim))
 test_x = csr_matrix((test_smp.element_x, (test_smp.row_index_x, test_smp.col_index_x)),
                     shape=(max(test_smp.row_index_x) + 1, n_dim))
+print('read data end')
 
 # perform tf-idf
+print(repr(time()) + ': tf-idf starting')
 train_tf_idf_x = sparse_tf_idf.tf_idf(train_x)
 test_tf_idf_x = sparse_tf_idf.tf_idf(test_x)
+print('tf-idf end')
 
 # fit mnb model
+print(repr(time()) + ': fit model starting')
 model = multi_label_mnb.fit(train_smp.y, train_tf_idf_x)
+print('fit model end')
 
 # make prediction on test sample
+print('make prediction starting')
 predict_sample_per_label = multi_label_mnb.predict(test_tf_idf_x, model)
+print('make prediction end')
 print(len(predict_sample_per_label))
 print(predict_sample_per_label.count(-1))
 print(time() - start_time)
