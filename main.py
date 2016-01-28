517fd8ebee01392512dd5958b329304777a37656
西伯利亚游牧人 <nomadcube@gmail.com>
2016-01-28 20:33:09 +0800

    分块预测

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import reader
 sample_path = sys.argv[1] if len(sys.argv) > 1 else '/Users/wumengling/PycharmProjects/kaggle/input_data/train.csv'
 size_of_sample = int(sys.argv[2]) if len(sys.argv) > 2 else 10000
 size_of_train_sample = int(sys.argv[3]) if len(sys.argv) > 3 else 5000
+predict_block_size = int(sys.argv[3]) if len(sys.argv) > 4 else 1000
 
 start_time = time()
 
@@ -27,7 +28,7 @@ test_x = csr_matrix((test_smp.element_x, (test_smp.row_index_x, test_smp.col_ind
 model = fit_multi_label_mnb.fit(train_smp.y, train_x)
 
 # make prediction on test sample
-predict_sample_per_label = predict_multi_label_mnb.predict(test_x, model, 2500)
+predict_sample_per_label = predict_multi_label_mnb.predict(test_x, model, predict_block_size)
 
 # evaluation
 print(evaluation.macro_precision_recall(test_smp.y, predict_sample_per_label, n_class_label))
