985c3296516fd1a3e4ded3d58cf94382f24d7530
西伯利亚游牧人 <nomadcube@gmail.com>
2016-02-03 16:19:00 +0800

    分train, test, part_train(仅用于测试模型效果)

--- main.py
+++ main.py
@@ -47,8 +47,8 @@ if __name__ == '__main__':
     start_time = time()
 
     sample_path = sys.argv[1] if len(sys.argv) > 1 else '/Users/wumengling/PycharmProjects/kaggle/input_data/train.csv'
-    size_test = int(sys.argv[2]) if len(sys.argv) > 2 else 100
-    size_train = int(sys.argv[3]) if len(sys.argv) > 3 else 1000
+    size_train = int(sys.argv[2]) if len(sys.argv) > 3 else 1000
+    size_test = int(sys.argv[3]) if len(sys.argv) > 2 else 100
     cnt_predict_class = int(sys.argv[4]) if len(sys.argv) > 4 else 1
 
     print(main(sample_path, size_train, size_test, cnt_predict_class))
