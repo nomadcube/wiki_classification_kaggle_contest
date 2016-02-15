c2d8d3e7accd4795a56df68a009c1c3b78f0b84e
西伯利亚游牧人 <nomadcube@gmail.com>
2016-02-15 18:02:58 +0800

    二元lr模型单元测试通过

--- models/lr.py
+++ models/lr.py
@@ -3,43 +3,50 @@ from random import randint
 import numpy as np
 
 
-def norm(w, q):
-    return abs(pow(sum(np.power(w, q)), 1. / q))
+def p_norm(w, p):
+    return abs(pow(sum(np.power(w, p)), 1. / p))
 
 
 def sigmoid(x):
     return 1. / (1. + np.exp((-1.) * x))
 
 
-def decision_function(w, x):
+def discrimination(w, x):
     return sum(w * x)
 
 
-def log_likelihood(w, y, x, regularization_coef, q):
-    if not isinstance(regularization_coef, float):
-        raise TypeError()
+def empirical_risk(w, y, x):
     res = 0.
     for i, each_y in enumerate(y):
         if each_y == 1:
-            res += np.log(sigmoid(decision_function(w, x[i])))
+            res += np.log(sigmoid(discrimination(w, x[i])))
         else:
-            res += np.log(1 - sigmoid(decision_function(w, x[i])))
-    return (-1.) * res + regularization_coef * norm(w, q)
+            res += np.log(1 - sigmoid(discrimination(w, x[i])))
+    return (-1.) * res
 
 
-def fit(y, x, regularization_coef, q):
-    init_w = np.repeat([0.1], x[0].shape[0])
-    return minimize(lambda w: log_likelihood(w, y, x, regularization_coef, q), init_w).x
+class LR:
+    def __init__(self, regularization_coefficient, p):
+        self._regularization_coefficient = regularization_coefficient
+        self._p = p
+        self.w = None
 
+    def fit(self, y, x):
+        self.w = self._solve(y, x, init_w=[0.1, 0.1, 0.1, 0.1, 0.1])
 
-def predict(x, estimated_w):
-    estimated_y = list()
-    for i, each_x in enumerate(x):
-        if sigmoid(decision_function(estimated_w, each_x)) < 0.5:
-            estimated_y.append(0)
-        else:
-            estimated_y.append(1)
-    return estimated_y
+    def predict(self, x):
+        estimated_y = list()
+        for i, each_x in enumerate(x):
+            if sigmoid(discrimination(self.w, each_x)) < 0.5:
+                estimated_y.append(0)
+            else:
+                estimated_y.append(1)
+        return estimated_y
+
+    def _solve(self, y, x, init_w):
+        return minimize(
+            lambda w: empirical_risk(w, y, x) + float(self._regularization_coefficient) * p_norm(w, self._p),
+            init_w).x
 
 
 if __name__ == '__main__':
@@ -47,9 +54,10 @@ if __name__ == '__main__':
     print x
     y = [randint(0, 1) for _ in range(10)]
     print y
-    estimated_w = fit(y, x, 0.3, 2)
-    print estimated_w
-    print fit(y, x, 0., 2)
-    print predict(x, estimated_w)
-    print norm([1, -2], 1)
-    print norm([1, -2], 2)
+    lr = LR(0, 2)
+    lr.fit(y, x)
+    print lr.w
+    print lr.predict(x)
+
+    print p_norm([1, -2], 1)
+    print p_norm([1, -2], 2)
---
+++ models/test_lr.py
@@ -0,0 +1,23 @@
+from array import array
+import lr
+
+
+class TestLR:
+    def pytest_funcarg__y(self):
+        return [0, 1, 0, 0, 0, 1, 0, 0, 1, 1]
+
+    def pytest_funcarg__x(self):
+        return [array('f', [2, 3, 3, 3, 2]), array('f', [2, 1, 1, 1, 3]), array('f', [1, 1, 2, 2, 3]),
+                array('f', [3, 1, 1, 1, 2]),
+                array('f', [2, 2, 3, 3, 2]), array('f', [1, 3, 2, 1, 1]), array('f', [1, 1, 2, 3, 1]),
+                array('f', [1, 1, 1, 1, 3]),
+                array('f', [3, 2, 1, 1, 3]), array('f', [3, 1, 2, 3, 3])]
+
+    def test_fit(self, y, x):
+        m = lr.LR(0, 2)
+        m.fit(y, x)
+        assert abs(m.w[0] - 0.22692853) < 1e-6
+        assert abs(m.w[1] - 16.43603803) < 1e-6
+        assert abs(m.w[2] - (-32.52328342)) < 1e-6
+        assert abs(m.w[3] - 15.67538028) < 1e-6
+        assert abs(m.w[4] - 0.084586) < 1e-6
