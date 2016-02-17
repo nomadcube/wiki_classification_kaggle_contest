55e18cc7913254c75fb84b03f652d5e3a6834ad5
西伯利亚游牧人 <nomadcube@gmail.com>
2016-02-17 19:47:58 +0800

    用较大的测试数据来运行main

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def main(in_path, threshold):
 
 if __name__ == '__main__':
     in_p = sys.argv[1] if len(
-        sys.argv) > 1 else '/Users/wumengling/PycharmProjects/kaggle/input_data/small_origin_train_subset.csv'
+        sys.argv) > 1 else '/Users/wumengling/PycharmProjects/kaggle/input_data/origin_train_subset.csv'
     t = float(sys.argv[2]) if len(sys.argv) > 2 else 99
 
     start_time = time()
