5008400dd149d47be200d0a37ec76ea76f480d0b
西伯利亚游牧人 <nomadcube@gmail.com>
2016-02-17 15:38:21 +0800

    修改labels, 使返回各label包含的instance索引号

--- data_analysis/labels.py
+++ data_analysis/labels.py
@@ -1,17 +1,22 @@
 from collections import namedtuple
 
 
-def occurrence_distribution(y):
-    occurrence_description = namedtuple("description", "cnt_total cnt_one max min")
-    occurrence = dict()
-    for labels in y:
+def occurrence(y):
+    res = dict()
+    for i, labels in enumerate(y):
         for each_label in labels:
-            occurrence.setdefault(each_label, 0)
-            occurrence[each_label] += 1.
-    return occurrence_description(len(occurrence), occurrence.values().count(1.), max(occurrence.values()),
-                                  min(occurrence.values()))
+            res.setdefault(each_label, set())
+            res[each_label].add(i)
+    return res
+
+
+def distribution(observations):
+    occurrence_description = namedtuple("description", "cnt_total cnt_one max min")
+    cnt = {k: len(v) for k, v in observations.items()}
+    return occurrence_description(len(cnt), cnt.values().count(1.), max(cnt.values()), min(cnt.values()))
 
 
 if __name__ == '__main__':
     y = [[314523, 165538, 416827], [21631], [76255, 165538]]
-    print occurrence_distribution(y)
+    print occurrence(y)
+    print distribution(occurrence(y))
