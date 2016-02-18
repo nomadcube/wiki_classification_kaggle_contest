050135148743786b9daa53e3989172257580027b
西伯利亚游牧人 <nomadcube@gmail.com>
2016-02-18 19:55:07 +0800

    fix: test_metrics.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from pipeline import PipeLine
 
 
 def main():
-    pl = PipeLine([97, 95, 93], [1.0, 0.0], [1, 2, 3])
     in_file = '/Users/wumengling/PycharmProjects/kaggle/input_data/small_origin_train_subset.csv'
-    pl.run(in_file)
     out_file = '/Users/wumengling/PycharmProjects/kaggle/output_data/submission.csv'
+    pl = PipeLine([97, 95], [1.0], [1, 2])
+    pl.run(in_file)
     submission(in_file, out_file, pl)
 
 
