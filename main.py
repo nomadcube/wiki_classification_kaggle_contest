e9c8c7cd587a3a0f7e3a894c4810f5159577948f
西伯利亚游牧人 <nomadcube@gmail.com>
2016-02-19 12:40:14 +0800

    完成mnb.predict多线程版本

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 from submission import submission
 from pipeline import PipeLine
 
-in_file = '/Users/wumengling/PycharmProjects/kaggle/input_data/origin_train_subset.csv'
-out_file = '/Users/wumengling/PycharmProjects/kaggle/output_data/submission.csv'
-pl = PipeLine([97, 95], [1.0], [3, 2])
-pl.run(in_file)
-# submission(in_file, out_file, pl)
-
 
 if __name__ == '__main__':
     import sys
@@ -15,8 +9,8 @@ if __name__ == '__main__':
     pr = cProfile.Profile()
     pr.enable()
 
-    in_file = sys.argv[0] if len(
-        sys.argv) > 0 else '/Users/wumengling/PycharmProjects/kaggle/input_data/origin_train_subset.csv'
+    in_file = sys.argv[1] if len(
+        sys.argv) > 1 else '/Users/wumengling/PycharmProjects/kaggle/input_data/origin_train_subset.csv'
     # out_file = '/Users/wumengling/PycharmProjects/kaggle/output_data/submission.csv'
     pl = PipeLine([97, 95], [1.0], [3, 2])
     pl.run(in_file)
