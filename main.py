c956d40d7cc8abbfe81fffcda4ab6f6e2d9f2c9a
西伯利亚游牧人 <nomadcube@gmail.com>
2016-02-19 12:38:02 +0800

    完成mnb.predict多线程版本

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ if __name__ == '__main__':
     pr = cProfile.Profile()
     pr.enable()
 
-    in_file = sys.argv[1] if len(
-        sys.argv) > 1 else '/Users/wumengling/PycharmProjects/kaggle/input_data/origin_train_subset.csv'
+    in_file = sys.argv[0] if len(
+        sys.argv) > 0 else '/Users/wumengling/PycharmProjects/kaggle/input_data/origin_train_subset.csv'
     # out_file = '/Users/wumengling/PycharmProjects/kaggle/output_data/submission.csv'
     pl = PipeLine([97, 95], [1.0], [3, 2])
     pl.run(in_file)
