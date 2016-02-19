af46dfb65e03bb45da2e9228cc180c5cdab1fa03
西伯利亚游牧人 <nomadcube@gmail.com>
2016-02-19 19:53:37 +0800

    修改read.py中提取测试集的方式： old: 对应2个或以上的label抽取第1条作为测试集 new: 对应3个或以上的label抽取前2条作为测试集
    原因：
    测试集中每个label只对应1条样本，得到的模型检验结果是悲观有偏的

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     in_file = sys.argv[1] if len(
         sys.argv) > 1 else '/Users/wumengling/PycharmProjects/kaggle/input_data/origin_train_subset.csv'
     # out_file = '/Users/wumengling/PycharmProjects/kaggle/output_data/submission.csv'
-    pipeline = PipeLine(LaplaceSmoothedMNB, [90], [5])
+    pipeline = PipeLine(LaplaceSmoothedMNB, [90, 80, 50], [5, 3])
 
     pr = cProfile.Profile()
     pr.enable()
--- read.py
+++ read.py
@@ -80,7 +80,8 @@ class Sample:
         label_occurrence = occurrence(self.y)
         common_label_cnt = 0.
         for label, instance_of_label in label_occurrence.items():
-            if len(instance_of_label) > 1:
+            if len(instance_of_label) > 2:
+                test_instances.add(instance_of_label.pop())
                 test_instances.add(instance_of_label.pop())
                 common_label_cnt += 1
         return test_instances, common_label_cnt
