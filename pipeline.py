9e9f37f534bedcf0460d6b422032df8c098eda27
西伯利亚游牧人 <nomadcube@gmail.com>
2016-02-19 15:58:48 +0800

    fix: 若mpr, mre任意一个为0， 会报错，修复

--- pipeline.py
+++ pipeline.py
@@ -2,16 +2,15 @@
 from itertools import product
 from read import Sample
 from preprocessing.transforming import YConverter, XConverter, convert_y_to_csr
-from models.mnb import SmoothedMNB
 from metrics import macro_precision_recall
 from preprocessing.tf_idf import tf_idf
 import sys
 
 
 class PipeLine:
-    def __init__(self, threshold, smooth_coef, predict_cnt):
+    def __init__(self, model_class, threshold, predict_cnt):
+        self._model = model_class
         self._threshold = threshold
-        self._smooth_coef = smooth_coef
         self._predict_cnt = predict_cnt
 
         self.best_f_score = 0.
@@ -29,8 +28,8 @@ class PipeLine:
         y_converter.construct(smp.y)
         mapped_y = y_converter.convert(train_smp.y)
 
-        for param in product(self._threshold, self._smooth_coef, self._predict_cnt):
-            tf_idf_threshold, mnb_smooth_coef, predict_cnt = param
+        for param in product(self._threshold, self._predict_cnt):
+            tf_idf_threshold, predict_cnt = param
 
             x_converter = XConverter(tf_idf_threshold)
             x_converter.construct(train_smp.x)
@@ -38,28 +37,31 @@ class PipeLine:
             mapped_reduced_x = tf_idf(x_converter.convert(train_smp.x))
             mapped_reduced_test_x = tf_idf(x_converter.convert(test_smp.x))
 
-            mnb = SmoothedMNB(mnb_smooth_coef)
+            mnb = self._model()
             mnb.fit(convert_y_to_csr(mapped_y), mapped_reduced_x)
 
             mapped_test_predicted_y = mnb.predict(mapped_reduced_test_x, predict_cnt)
             print mapped_test_predicted_y
-            # mapped_test_y = y_converter.convert(test_smp.y)
-            #
-            # mpr_mre = macro_precision_recall(mapped_test_y, mapped_test_predicted_y)
-            # f_score = 1. / (1. / mpr_mre[0] + 1. / mpr_mre[1])
-            #
-            # if f_score > self.best_f_score:
-            #     self.best_f_score = round(f_score, 3)
-            #     self.best_model = mnb
-            #     self.best_x_converter = x_converter
-            #     self.best_y_converter = y_converter
-            #     self.best_predicted_cnt = predict_cnt
+            mapped_test_y = y_converter.convert(test_smp.y)
+
+            mpr_mre = macro_precision_recall(mapped_test_y, mapped_test_predicted_y)
+            f_score = 1. / (1. / mpr_mre[0] + 1. / mpr_mre[1]) if mpr_mre[0] != 0. and mpr_mre[1] != 0. else float(
+                "inf")
+
+            if f_score > self.best_f_score:
+                self.best_f_score = round(f_score, 3)
+                self.best_model = mnb
+                self.best_x_converter = x_converter
+                self.best_y_converter = y_converter
+                self.best_predicted_cnt = predict_cnt
 
 
 if __name__ == '__main__':
+    from models.mnb import LaplaceSmoothedMNB
+
     PATH = sys.argv[1] if len(
         sys.argv) > 1 else '/Users/wumengling/PycharmProjects/kaggle/input_data/small_origin_train_subset.csv'
-    cv = PipeLine([97, 95, 93], [1.0], [1, 2, 3])
+    cv = PipeLine(LaplaceSmoothedMNB, [97, 95, 93], [1, 2, 3])
     cv.run(PATH)
     print cv.best_predicted_cnt
     print cv.best_f_score
