1c1cdc637fcb234215a158e22fcc4f6377c13da2
西伯利亚游牧人 <nomadcube@gmail.com>
2016-02-24 16:46:25 +0800

    增量模型完成

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     # pr.enable()
     t = time()
 
-    pipeline.run(train_file, 100)  # 分块大小和预测目标个数的乘积必须小于总样本量
+    pipeline.run(train_file, 1000)  # 分块大小和预测目标个数的乘积必须小于总分类数
     print repr(pipeline)
     # submission(exam_file, exam_out_file, pipeline)
 
