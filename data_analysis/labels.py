611f6a3319ddabb48f54f1daaf21b1deab6fbbbd
西伯利亚游牧人 <nomadcube@gmail.com>
2016-03-13 16:24:37 +0800

    修改针对label的数据分析

--- data_analysis/labels.py
+++ data_analysis/labels.py
@@ -1,31 +1,8 @@
-from collections import namedtuple, defaultdict
+from collections import defaultdict
 
 
 def occurrence(y):
     res = defaultdict(set)
     for i, labels in enumerate(y):
-        for j, each_label in enumerate(labels):
-            res[each_label].add(i)
+        res[labels].add(i)
     return res
-
-
-def distribution(observations):
-    occurrence_description = namedtuple("description", "cnt_total cnt_one max min")
-    cnt = {k: len(v) for k, v in observations.items()}
-    return occurrence_description(len(cnt), cnt.values().count(1.), max(cnt.values()), min(cnt.values()))
-
-
-def most_frequent_label(y, top_k):
-    occurrence_cnt = {k: len(v) for k, v in occurrence(y).items()}
-    return sorted(occurrence_cnt, key=lambda x: occurrence_cnt[x], reverse=True)[:top_k]
-
-
-if __name__ == '__main__':
-    y = [[314523, 165538, 416827], [21631], [76255, 165538]]
-    oy = occurrence(y)
-    print oy
-    print distribution(oy)
-    for label, instances in oy.items():
-        if len(instances) > 1:
-            print instances.pop()
-    print oy
