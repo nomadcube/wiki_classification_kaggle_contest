a476d3eeba8e826612ef6e373d8d3411c7fe4292
西伯利亚游牧人 <nomadcube@gmail.com>
2016-03-13 16:23:53 +0800

    将cnb作为一个新类抽离出来

--- models/mnb.py
+++ models/mnb.py
@@ -4,7 +4,7 @@ import numpy as np
 from scipy.sparse import csc_matrix
 from memory_profiler import profile
 
-from preprocessing.transforming import convert_y_to_csr
+from transformation.converter import convert_y_to_csr
 
 
 class LaplaceSmoothedMNB:
@@ -17,22 +17,34 @@ class LaplaceSmoothedMNB:
         self.b = self.estimate_b(y_train_csr)
         self.w = self.estimate_w(y_train_csr, train_x)
 
-    def predict(self, test_x, predict_cnt=1):
-        """
-        :param test_x: csr矩阵
-        :param predict_cnt: int
-        :return: test_x的各个instance对应的predict_cnt个预测结果
+    def predict(self, x):
+        prob = self.post_prob(x)
+        return [[pred] for pred in np.argmax(prob, axis=1)]
 
-        当test_x为展开label的状态，即如314523,165538,416827 1250536:1这一行会展开为3个相同的instance， 这3个instance的预测结果是一样的
-        这并不影响模型评估指标的计算，因为这相当于将预测结果按instance group by 之后，以其中的一个预测结果作为group by 后的预测结果
-        """
-        prediction = list()
-        log_likelihood_mat = test_x.dot(self.w.transpose())
-        for i, each_x in enumerate(log_likelihood_mat):
-            post_probability = (-1.) * np.array(each_x.todense())[0] + self.b
-            prediction.append([label for label in self.top_k_label(post_probability, predict_cnt)])
-        return prediction
+    def post_prob(self, x):
+        prob = x.dot(self.w.transpose())
+        return self.b.take(np.diff(prob.indices)) + prob
 
+    @staticmethod
+    def estimate_w(y, x):
+        label_feature_coef = y.dot(x).todense()
+        label_feature_coef += 1.0
+        label_sum = np.array(label_feature_coef.sum(axis=1).ravel())[0]
+        label_feature_coef = label_feature_coef.transpose()
+        label_feature_coef /= label_sum
+        label_feature_coef = np.log(label_feature_coef)
+        return csc_matrix(label_feature_coef.transpose())
+
+    @staticmethod
+    def estimate_b(y):
+        each_label_occurrence = np.array(y.sum(axis=1).ravel())[0]
+        total_occurrence = each_label_occurrence.sum()
+        each_label_occurrence /= total_occurrence
+        each_label_occurrence = np.log(each_label_occurrence)
+        return each_label_occurrence
+
+
+class CNB(LaplaceSmoothedMNB):
     @staticmethod
     def estimate_w(y, x):
         y_x_param = y.dot(x).todense()
@@ -46,14 +58,6 @@ class LaplaceSmoothedMNB:
             y_x_param.shape)
         return csc_matrix(contrast_y_x_param.transpose())
 
-    @staticmethod
-    def estimate_b(y):
-        each_label_occurrence = np.array(y.sum(axis=1).ravel())[0]
-        total_occurrence = each_label_occurrence.sum()
-        each_label_occurrence /= total_occurrence
-        each_label_occurrence = np.log(each_label_occurrence)
-        return each_label_occurrence
-
-    @staticmethod
-    def top_k_label(arr, k):
-        return np.argsort(arr)[-k:]
+    def post_prob(self, x):
+        prob = x.dot(self.w.transpose())
+        return self.b.take(np.diff(prob.indices)) + (-1.) * prob
