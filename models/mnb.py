84f5ca6afa9be6cfbe904490b42e03ada1d3c726
西伯利亚游牧人 <nomadcube@gmail.com>
2016-03-14 16:51:30 +0800

    修改： 1. 修正cnb算法

--- models/mnb.py
+++ models/mnb.py
@@ -2,7 +2,7 @@
 import numpy as np
 
 from copy import deepcopy
-from scipy.sparse import csc_matrix
+from scipy.sparse import csc_matrix, csr_matrix
 from memory_profiler import profile
 
 from transformation.converter import convert_y_to_csr
@@ -46,24 +46,21 @@ class LaplaceSmoothedMNB:
 
 
 class CNB(LaplaceSmoothedMNB):
-    def fit(self, train_y, train_x, smp_weight=None):
-        y_train_csr = convert_y_to_csr(train_y)
-        self.b = self.estimate_b(y_train_csr)
-        turned_train_y = list()
-        for each_label in train_y:
-            new_label = None
-            if each_label != 0:
-                new_label = 0
-            if each_label != 1:
-                new_label = 1
-            if each_label != 2:
-                new_label = 2
-            turned_train_y.append(new_label)
-        self.w = self.estimate_w(y_train_csr, train_x)
-
     def post_prob(self, x):
         prob = (-1.) * x.dot(self.w.transpose()).todense() + self.b
         return prob
+
+    @staticmethod
+    def estimate_w(y, x):
+        new_y = y.todense()
+        new_y = csr_matrix(1.0 - new_y)
+        label_feature_coef = new_y.dot(x).todense()
+        label_feature_coef += 1.0
+        label_sum = np.array(label_feature_coef.sum(axis=1).ravel())[0]
+        label_feature_coef = label_feature_coef.transpose()
+        label_feature_coef /= label_sum
+        label_feature_coef = np.log(label_feature_coef)
+        return csc_matrix(label_feature_coef.transpose())
 
 
 class NonSmoothedMNB(LaplaceSmoothedMNB):
